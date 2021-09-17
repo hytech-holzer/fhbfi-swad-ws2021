60c526b033f92d0b6e94525904e5dfa954e3ce6b
Markus Holzer <markus.holzer@hytech.at>
2021-09-17 16:11:28 +0200

    init covid-test-api commit

---
+++ src/main/java/at/ac/fhbfi/covidtestapi/CovidTestApiApplication.java
@@ -0,0 +1,13 @@
+package at.ac.fhbfi.covidtestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidTestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/ac/fhbfi/covidtestapi/CovidTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.fhbfi.covidtestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
